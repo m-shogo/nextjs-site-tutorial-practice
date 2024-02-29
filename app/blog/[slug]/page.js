b4a94b29d7f184dcf3d02a996c7bcc9dd75f1e5d
matsuzaki <matsuzaki@codia.co.jp>
2024-02-29 12:23:14 +0900

    記事取得{ getAllBlogs, getSingleBlog }共通化

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,18 +1,6 @@
 import Image from "next/image";
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
 import ReactMarkdown from "react-markdown";
-
-async function getSingleBlog(context) {
-  const { slug } = context.params;
-  const data = await import(`../../../data/${slug}.md`);
-  const singleDocument = matter(data.default);
-
-  return {
-    singleDocument: singleDocument,
-  };
-}
+import { getAllBlogs, getSingleBlog } from "../../utils/mdQueries" 
 
 const SingleBlog = async (props) => {
   const { singleDocument } = await getSingleBlog(props);
@@ -42,21 +30,6 @@ const SingleBlog = async (props) => {
 export default SingleBlog;
 
 export async function generateStaticParams() {
-  async function getAllBlogs() {
-    const files = fs.readdirSync(path.join("data"));
-    const blogs = files.map((fileName) => {
-      const slug = fileName.replace(".md", "");
-      const fileData = fs.readFileSync(path.join("data", fileName), "utf-8");
-      const { data } = matter(fileData);
-      return {
-        frontmatter: data,
-        slug: slug,
-      };
-    });
-    return {
-      blogs: blogs,
-    };
-  }
   const { blogs } = await getAllBlogs();
   const paths = blogs.map((blog) => `/${blog.slug}`);
   return paths;
